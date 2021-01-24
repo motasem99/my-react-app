c0b06c0fb58d1bc308d0db642bf5350b79008367
mutasem <motasemimad10@gmail.com>
2021-01-24 11:51:26 +0200

    finish form

--- src/pages/Checkout.jsx
+++ src/pages/Checkout.jsx
@@ -6,7 +6,7 @@ import BurgerIngredients from '../components/BurgerIngredients/BurgerIngredients
 const Checkout = (props) => (
     <div className={classes.Main}>
         <div className={classes.Burger}>
-        <h2>We hope it taste is well</h2>
+        <h2>We Hope It Taste Is Well</h2>
             <BurgerIngredients type='bread-top' />
             <BurgerIngredients type='salad' />
             <BurgerIngredients type='bacon' />
@@ -17,6 +17,21 @@ const Checkout = (props) => (
 
         <button className={classes.Success}>CANCEL</button>
         <button className={classes.Danger}>CONTINUE</button>
+        <div>
+            <form className={classes.Form}>
+            <h3>Enter Your Contact Data</h3>
+                <input type="text" placeholder="Your Name" />
+                <input type="text" placeholder="Street" />
+                <input type="text" placeholder="ZIP Code" />
+                <input type="text" placeholder="Country" />
+                <input type="text" placeholder="Your E-Mail" />
+                <select>
+                    <option>Fastest</option>
+                    <option>Cheapest</option>
+                </select>
+                <button>ORDER</button>
+            </form>
+        </div>
     </div>
 )
 
