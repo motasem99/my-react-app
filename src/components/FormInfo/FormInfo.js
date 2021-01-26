3d8a5294c3e27752c425068da827d25f1a878d89
mutasem <motasemimad10@gmail.com>
2021-01-26 19:16:04 +0200

    handle form

--- src/components/FormInfo/FormInfo.js
+++ src/components/FormInfo/FormInfo.js
@@ -7,24 +7,26 @@ const FormInfo = (props) => {
     const {infoForm} = props
 
     return (
-        <div>
-            <form onSubmit={infoForm} className={classes.Form}>
-                <h3>Enter Your Contact Data</h3>
-                {
-                    props.error &&
-                    <div className={classes.Error}>{props.nameOfInput} is missing</div>
-                }
-                <input type="text" name="name" placeholder="Your Name" />
-                <input type="text" name="street" placeholder="Street" />
-                <input type="text" name="zipCode" placeholder="ZIP Code" />
-                <input type="text" name="country" placeholder="Country" />
-                <input type="text" name="mail" placeholder="Your E-Mail" />
-                <select>
-                    <option>Fastest</option>
-                    <option>Cheapest</option>
-                </select>
-                <button type="submit">ORDER</button>
-            </form>
+        <div className={classes.DropForm }>
+            <div className={classes.Form}>
+                <form onSubmit={infoForm}>
+                    <h3>Enter Your Contact Data</h3>
+                    {
+                        props.error &&
+                        <div className={classes.Error}>{props.nameOfInput} is missing</div>
+                    }
+                    <input type="text" name="name" placeholder="Your Name" />
+                    <input type="text" name="street" placeholder="Street" />
+                    <input type="text" name="zipCode" placeholder="ZIP Code" />
+                    <input type="text" name="country" placeholder="Country" />
+                    <input type="text" name="mail" placeholder="Your E-Mail" />
+                    <select>
+                        <option>Fastest</option>
+                        <option>Cheapest</option>
+                    </select>
+                    <button type="submit">ORDER</button>
+                </form>
+            </div>
         </div>
         )
     }
