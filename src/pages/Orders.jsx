17ce9975bcbf42e3b4d96439b4e4a39dfac8fd15
mutasem <motasemimad10@gmail.com>
2021-01-28 17:30:03 +0200

    finished order component

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -6,44 +6,58 @@ import firebase from '../firebase.js';
 class Orders extends Component {
   state = {
     ingredients: [],
+    loading: true,
   };
 
   componentDidMount() {
     firebase
       .database()
-      .ref('ingredients')
+      .ref(`ingredients/1`)
       .on('value', (snapshot) => {
-        this.setState({
-          ingredients: snapshot.val().ingredients,
-          price: snapshot.val().price,
-        });
+        const data = Object.keys(snapshot.val()).map((item) => ({
+          id: item,
+          ...snapshot.val()[item],
+        }));
+        this.setState({ ingredients: [...data], loading: false });
       });
   }
 
   render() {
-    return (
-      <div className={classes.Main}>
-        <p>
-          Ingredients:
-          <span className={classes.Span}>
-            Bacon({this.state.ingredients.bacon})
-          </span>
-          <span className={classes.Span}>
-            Cheese({this.state.ingredients.cheese})
-          </span>
-          <span className={classes.Span}>
-            Meat({this.state.ingredients.meat})
-          </span>
-          <span className={classes.Span}>
-            Salad({this.state.ingredients.salad})
-          </span>
-        </p>
-        <p>
-          Price: <strong>USD {this.state.price}</strong>
-        </p>
-      </div>
+    return this.state.loading ? (
+      <div>loading....</div>
+    ) : (
+      [
+        this.state.ingredients.map((item) => (
+          <div className={classes.Main} key={item.id}>
+            <p>
+              Ingredients:
+              <span className={classes.Span}>
+                Bacon({item.ingredients.bacon})
+              </span>
+              <span className={classes.Span}>
+                Cheese({item.ingredients.cheese})
+              </span>
+              <span className={classes.Span}>
+                Meat({item.ingredients.meat})
+              </span>
+              <span className={classes.Span}>
+                Salad({item.ingredients.salad})
+              </span>
+            </p>
+            <p>
+              Price: <strong>USD {item.price.toFixed(2)}</strong>
+            </p>
+          </div>
+        )),
+      ]
     );
   }
 }
 
 export default Orders;
+
+/**
+ * 1. when u got data u need to wait use gui by use loading state
+ * 2. when u got data from firebase u need to change it from object to arr
+ * 3. use map with GUI
+ */
